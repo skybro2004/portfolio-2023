07404e6a78d1df1b23d70f3b8d3d93935765734e
skybro2004 <skybro2004@gmail.com>
2022-08-08 01:14:35 +0900

    asdf

--- js/index.js
+++ js/index.js
@@ -4,10 +4,8 @@ typoElem.addEventListener("animationstart", function(){
         typoElem.innerHTML = "FULLSTACK"
     }, 1000)
 })
-console.log(typoElem.nextSibling);
 typoElem.nextElementSibling.addEventListener("animationstart", function(){
     setTimeout(function(){
-        console.log("asdf");
         typoElem.nextElementSibling.innerHTML = "PROGRAMMER"
     }, 1000)
 })
