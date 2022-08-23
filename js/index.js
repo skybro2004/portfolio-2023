942ef6f715ae4e1370d440b68393900f09ff026e
skybro2004 <skybro2004@gmail.com>
2022-08-23 09:39:35 +0900

    asdf

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ typoElem.nextElementSibling.addEventListener("animationstart", function(){
         typoElem.nextElementSibling.innerHTML = "PROGRAMMER"
     }, 1000)
 })
+
+
+
+// function dropdown(elem){
+//     elem.children[2].classList.toggle("show")
+// }
+
+let dropdown = document.getElementsByClassName("btn-dropdown")
+
+for(let elem of dropdown){
+    console.log(elem);
+
+    elem.addEventListener("click", (e) => {
+        console.log(e);
+        elem.children[2].classList.toggle("show")
+    })
+
+    elem.addEventListener("blur", (e) => {
+        console.log(e);
+        elem.children[2].classList.remove("show")
+    })
+}
